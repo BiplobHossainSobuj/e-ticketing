c61f839d49c1f747c4e4e51d2febcb99295b0f0f
BiploHossainSobuj <bhs.cse@outlook.com>
2024-02-19 13:09:17 +0600

    make price and coupon field functional

--- scripts/ticketing.js
+++ scripts/ticketing.js
@@ -1,9 +1,11 @@
 function getSeat() {
     const seats = document.getElementsByClassName('seat')
     let selectedSeat = 0;
+    let totalTicketPrice = 0;
     for (const seat of seats) {
         seat.addEventListener('click', function (event) {
             selectedSeat += 1;
+            totalTicketPrice+=550;
             if (selectedSeat <= 4) {
                 const currentSeat = document.getElementById('current-seat');
                 currentSeat.innerText = selectedSeat;
@@ -11,6 +13,31 @@ function getSeat() {
                 const currentSeatLeft = parseInt(currentSeatLeftText.innerText);
                 currentSeatLeftText.innerText = currentSeatLeft - 1;
                 seat.classList.add('bg-amber-400');
+                // append seat 
+                const selectedSeatDiv = document.getElementById('selected-seat-container');
+                const p1 = document.createElement('p');
+                p1.innerText = event.target.innerText;
+                selectedSeatDiv.appendChild(p1);
+                // append seat class 
+                const selectedSeatClass = document.getElementById('selected-seat-class');
+                const p2 = document.createElement('p');
+                p2.innerText = 'Economy';
+                selectedSeatClass.appendChild(p2);
+                //Append price
+                const seatPrice = document.getElementById('selected-seat-price');
+                const p3 = document.createElement('p');
+                p3.innerText = 550;
+                seatPrice.appendChild(p3);
+                //update total ticket price
+                const totalTicketPriceText = document.getElementById('selected-ticket-price');
+                totalTicketPriceText.innerText = totalTicketPrice;
+                //update grand total
+                const grandTotalText = document.getElementById('grand-total-price');
+                grandTotalText.innerText = totalTicketPrice;
+                //apply coupon
+                
+                // const couponField = document.getElementById('coupon-field');
+                // console.log(couponField.value);
             }else{
                 alert(`You have selected maximum number of seat 4`);
             }
--- scripts/utility.js
+++ scripts/utility.js
@@ -4,4 +4,25 @@
     return element;
  }
 
- 
+ function getDiscount(){
+    const couponField = document.getElementById('coupon-field');
+    const couponValue = couponField.value;
+    const priceText = document.getElementById('selected-ticket-price');
+    const priceWithoutDiscount = parseFloat(priceText.innerText);
+    const grandTotalText = document.getElementById('grand-total-price');
+    let grandTotalAmount = parseFloat(grandTotalText.innerText);
+    if(couponValue==='NEW15' || couponValue ==='Couple 20'){
+        if (couponValue==='NEW15') {
+            const discountAmount = priceWithoutDiscount * 0.15;
+            grandTotalText.innerText = grandTotalAmount - discountAmount;
+            document.getElementById('coupon-container').classList.add('hidden');
+        }
+        else if(couponValue ==='Couple 20'){
+            const discountAmount = priceWithoutDiscount * 0.2;
+            grandTotalText.innerText = grandTotalAmount - discountAmount;
+            document.getElementById('coupon-container').classList.add('hidden');
+        }
+    }
+    
+ }
+ getDiscount();
